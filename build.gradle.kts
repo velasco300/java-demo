d6c9cb104c4fb7d73213ffa496021890b23903f4
hello <hello@hello.com>
2019-04-21 11:15:35 +0800

    初始化代码模板

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    id("org.springframework.boot") version "2.1.4.RELEASE"
+    id("java")
+}
+
+group = "com.aaa"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    val springCloudVersion: String by project
+    
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
